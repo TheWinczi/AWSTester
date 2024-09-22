48a2479f05844237f409376410dab75297146a75
TheWinczi <mwinczewski@tutanota.com>
2024-09-22 09:37:47 +0200

    Solves deployment issues

--- mysite/gunicorn.py
+++ mysite/gunicorn.py
@@ -1,7 +1,7 @@
 from mysite import settings
 
 # Classic Gunicorn variables
-command = settings.BASE_DIR / 'venv' / 'bin' / 'gunicorn'
-pythonpath = settings.BASE_DIR
-bind = settings.BINF
+command = str(settings.BASE_DIR / 'venv' / 'bin' / 'gunicorn')
+pythonpath = str(settings.BASE_DIR)
+bind = settings.BIND
 workers = settings.WORKERS
